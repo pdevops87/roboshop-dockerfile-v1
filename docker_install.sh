d31dffa461969ac5c612ac97e69534908a399943
Your Name <vemasani.pavani@gmail.com>
2025-12-26 03:13:48 +0530

    install docker

---
+++ docker_install.sh
@@ -0,0 +1,21 @@
+res=$1
+if [ "$res" = "install"  ]; then
+ sudo dnf -y install dnf-plugins-core
+ sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+ sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+ sudo systemctl enable docker
+ sudo systemctl start docker
+fi
+
+
+if [ "$res" = "uninstall" ]; then
+   sudo dnf remove -y dnf-plugins-core
+   sudo rm -f /etc/yum.repos.d/docker-ce.repo
+   sudo dnf remove -y \
+      docker-ce \
+      docker-ce-cli \
+      containerd.io \
+      docker-buildx-plugin \
+      docker-compose-plugin
+fi
+
